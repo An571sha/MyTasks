7317fbbcfff19c19455bb0b3b6038671da2cfed1
an571sha <anim_esh@yahoo.co.in>
2019-06-05 14:59:58 +0300

    ++ create methods for generating location and Tags tags ++ ++ fixed the xml data structure ++ ++ fixed the tags not showing multiple entries ++

---
+++ EvernoteImport/src/main/java/Tags.java
@@ -0,0 +1,11 @@
+public class Tags {
+
+    public String tags;
+    public String title;
+
+    public Tags(String tags, String title) {
+        this.tags = tags;
+        this.title = title;
+    }
+
+}
